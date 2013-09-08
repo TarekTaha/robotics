5d8624cce239c88cf474f6e13eb34cbf7f035c03
Tarek Taha <tarek@tarektaha.com>
2013-09-08 19:55:36 +1000

    - script update

--- ROSCode/sketchbook/updateCode.py
+++ ROSCode/sketchbook/updateCode.py
@@ -1,9 +1,15 @@
 #!/bin/python
+import time
 import subprocess
-#import git 
 
-#g = git.cmd.Git('.')
-#g.pull()
+while True:
+  print "This prints once a minute."
+  process = subprocess.Popen(["git", "pull"], stdout=subprocess.PIPE)
+  output = process.communicate()[0]
+  if output != '' and 'Already up-to-date' not in output:
+    print 'Code Changed, I should recompile and upload to the Atmega328 board'
+  else: 
+    print 'Nothing changed'
+  time.sleep(60)  # Delay for 1 minute (60 seconds)
+  
 
-process = subprocess.Popen(["git", "pull"], stdout=subprocess.PIPE)
-output = process.communicate()[0]
