3cf1ec5da10654618ff38426f8fb54e70aff90a5
Tarek Taha <tarek@tarektaha.com>
2013-09-15 19:13:44 +1000

    - sonar

--- ROSCode/sketchbook/robotica/src/sketch.ino
+++ ROSCode/sketchbook/robotica/src/sketch.ino
@@ -76,13 +76,13 @@ void move(double distance,int speed);
 // ROS Related
 std_msgs::Float32 sonar_msg;
 std_msgs::Float32 motorVolt_msg;
-std_msgs::Int32   encoder1_msg;
-std_msgs::Int32   encoder2_msg;
+//std_msgs::Int32   encoder1_msg;
+//std_msgs::Int32   encoder2_msg;
 geometry_msgs::TransformStamped robotPoseMsg;
 
 ros::Publisher pub_sonar    ("sonar"   ,  &sonar_msg);
-ros::Publisher pub_encoder1 ("encoder1",  &encoder1_msg);
-ros::Publisher pub_encoder2 ("encoder2",  &encoder2_msg);
+//ros::Publisher pub_encoder1 ("encoder1",  &encoder1_msg);
+//ros::Publisher pub_encoder2 ("encoder2",  &encoder2_msg);
 ros::Publisher pub_motorVolt("motorVolt", &motorVolt_msg);
 ros::NodeHandle  nh;
 //tf::TransformBroadcaster robotPoseBroadcaster;
